c2b0d8be783f89b0ecb2836d592844d5b7605d5e
Norizon <norizontunes@gmail.com>
2025-11-25 13:13:09 +0700

    feat(firefox): add policies and preferences

--- home/browsers/firefox.nix
+++ home/browsers/firefox.nix
@@ -1,7 +1,76 @@
+{ config, pkgs, ... }:
+let
+    lock-false = {
+        Value = false;
+        Status = "locked";
+    };
+    lock-true = {
+        Value = true;
+        Status = "locked";
+    };
+in
 {
     programs.firefox = {
         enable = true;
-        profiles.main = {};
+        profiles = {
+            main = {
+                id = 0;
+                isDefault = true;
+            };
+
+            school = {
+                id = 1;
+            };
+        };
+        policies = {
+            DisableTelemetry = true;
+            DisableFirefoxStudies = true;
+            EnableTrackingProtection = {
+                Value= true;
+                Locked = true;
+                Cryptomining = true;
+                Fingerprinting = true;
+            };
+            DisablePocket = true;
+            DisableFirefoxAccounts = true;
+            DisableAccounts = true;
+            DisableFirefoxScreenshots = true;
+            OverrideFirstRunPage = "";
+            OverridePostUpdatePage = "";
+            DontCheckDefaultBrowser = true;
+            DisplayBookmarksToolbar = "never"; # alternatives: "always" or "newtab"
+            DisplayMenuBar = "default-off"; # alternatives: "always", "never" or "default-on"
+            SearchBar = "unified"; # alternative: "separate"
+
+            ExtensionSettings = {
+            "*".installation_mode = "blocked";
+                "uBlock0@raymondhill.net" = {
+                    install_url = "https://addons.mozilla.org/firefox/downloads/latest/ublock-origin/latest.xpi";
+                    installation_mode = "force_installed";
+                };
+            };
+    
+            Preferences = { 
+                "browser.contentblocking.category" = { Value = "strict"; Status = "locked"; };
+                "extensions.pocket.enabled" = lock-false;
+                "extensions.screenshots.disabled" = lock-true;
+                "browser.topsites.contile.enabled" = lock-false;
+                "browser.formfill.enable" = lock-false;
+                "browser.search.suggest.enabled" = lock-false;
+                "browser.search.suggest.enabled.private" = lock-false;
+                "browser.urlbar.suggest.searches" = lock-false;
+                "browser.urlbar.showSearchSuggestionsFirst" = lock-false;
+                "browser.newtabpage.activity-stream.feeds.section.topstories" = lock-false;
+                "browser.newtabpage.activity-stream.feeds.snippets" = lock-false;
+                "browser.newtabpage.activity-stream.section.highlights.includePocket" = lock-false;
+                "browser.newtabpage.activity-stream.section.highlights.includeBookmarks" = lock-false;
+                "browser.newtabpage.activity-stream.section.highlights.includeDownloads" = lock-false;
+                "browser.newtabpage.activity-stream.section.highlights.includeVisited" = lock-false;
+                "browser.newtabpage.activity-stream.showSponsored" = lock-false;
+                "browser.newtabpage.activity-stream.system.showSponsored" = lock-false;
+                "browser.newtabpage.activity-stream.showSponsoredTopSites" = lock-false;
+            };
+        };
     };
 
     stylix.targets.firefox.profileNames = [ "main" ];
