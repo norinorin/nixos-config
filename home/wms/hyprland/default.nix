63f2a19c87658a8a53f249cc527e33cb3a49de47
Norizon <norizontunes@gmail.com>
2025-11-26 02:04:59 +0700

    fix(hyprland): make gamemode executable

--- home/wms/hyprland/default.nix
+++ home/wms/hyprland/default.nix
@@ -184,7 +184,7 @@
                 "ALT SHIFT , Tab, cyclenext, prev"
                 "ALT SHIFT , Tab, bringactivetotop"
 
-                "$mod      , F1, exec, ~/.config/hypr/gamemode.sh"
+                "ALT       , F1, exec, ~/.config/hypr/gamemode.sh"
 
                 # OBS
                 "CTRL ALT  , apostrophe, pass, class:com.obsproject.Studio"
@@ -285,20 +285,23 @@
         '';
     };
 
-    home.file.".config/hypr/gamemode.sh".text = ''
-        #!/usr/bin/env sh
-        HYPRGAMEMODE=$(hyprctl getoption animations:enabled | awk 'NR==1{print $2}')
-        if [ "$HYPRGAMEMODE" = 1 ] ; then
-            hyprctl --batch "\
-                keyword animations:enabled 0;\
-                keyword decoration:shadow:enabled 0;\
-                keyword decoration:blur:enabled 0;\
-                keyword general:gaps_in 0;\
-                keyword general:gaps_out 0;\
-                keyword general:border_size 1;\
-                keyword decoration:rounding 0"
-            exit
-        fi
-        hyprctl reload
-    '';
+    home.file.".config/hypr/gamemode.sh" = {
+        text = ''
+            #!/usr/bin/env sh
+            HYPRGAMEMODE=$(hyprctl getoption animations:enabled | awk 'NR==1{print $2}')
+            if [ "$HYPRGAMEMODE" = 1 ] ; then
+                hyprctl --batch "\
+                    keyword animations:enabled 0;\
+                    keyword decoration:shadow:enabled 0;\
+                    keyword decoration:blur:enabled 0;\
+                    keyword general:gaps_in 0;\
+                    keyword general:gaps_out 0;\
+                    keyword general:border_size 1;\
+                    keyword decoration:rounding 0"
+                exit
+            fi
+            hyprctl reload
+        '';
+        executable = true;
+    };
 }
