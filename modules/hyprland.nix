331b71266f65c26b550952fc2f362207a33800ff
Norizon <norizontunes@gmail.com>
2025-11-26 10:20:57 +0700

    fix(hyprland): spoof current desktop to X-NIXOS-SYSTEMD-AWARE

--- modules/hyprland.nix
+++ modules/hyprland.nix
@@ -4,6 +4,9 @@ let
     pkgs-unstable = inputs.hyprland.inputs.nixpkgs.legacyPackages.${pkgs.stdenv.hostPlatform.system};
 in
 {
+    # https://github.com/NixOS/nixpkgs/pull/297434#issuecomment-2348783988
+    systemd.services.display-manager.environment.XDG_CURRENT_DESKTOP = "X-NIXOS-SYSTEMD-AWARE";
+    
     programs.hyprland = {
         enable = true;
         package = pkgs-hypr.hyprland;
