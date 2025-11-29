fc6214f21bdc6575688071e2279acf07ac242062
Nori <norizon16@proton.me>
2025-11-30 00:05:38 +0700

    remove swap partition

--- nixos/hardware-configuration.nix
+++ nixos/hardware-configuration.nix
@@ -4,7 +4,6 @@
 {
   config,
   lib,
-  pkgs,
   modulesPath,
   ...
 }: {
@@ -27,10 +26,6 @@
     fsType = "vfat";
     options = ["fmask=0022" "dmask=0022"];
   };
-
-  swapDevices = [
-    {device = "/dev/disk/by-uuid/1b7e26c7-a8af-4c5d-ab2c-4cb5a90a64fa";}
-  ];
 
   # Enables DHCP on each ethernet and wireless interface. In case of scripted networking
   # (the default) this is the recommended approach. When using systemd-networkd it's
