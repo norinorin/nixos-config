d168eaf54c30fa6249c8544200447a19ba426d32
Nori <norizon16@proton.me>
2025-12-01 08:47:15 +0700

    wlogout: change layout
    Use kill-session for logout and get rid of suspend and lock

--- home/wms/default.nix
+++ home/wms/default.nix
@@ -11,11 +11,58 @@
     cage
     gamescope
     grim
-    wlogout
   ];
 
   programs.waybar = {
     enable = true;
     systemd.enable = false;
+  };
+
+  # get rid of suspend as we can't even suspend with nvidia anyway
+  # also get rid of lock cos it's more practical to use Sup+Alt L to lock via compositor
+  # layout:
+  # H | S | R
+  #   | E |
+  programs.wlogout = {
+    enable = true;
+    layout = [
+      {
+        label = "hibernate";
+        action = "systemctl hibernate";
+        text = "Hibernate";
+        keybind = "h";
+      }
+      {
+        label = "";
+        action = "true";
+        text = "";
+        keybind = "";
+      }
+      {
+        label = "shutdown";
+        action = "systemctl poweroff";
+        text = "Shutdown";
+        keybind = "s";
+      }
+      {
+        label = "logout";
+        # use kill-session otherwise we'll never get back to sddm
+        action = "loginctl kill-session $XDG_SESSION_ID";
+        text = "Logout";
+        keybind = "e";
+      }
+      {
+        label = "reboot";
+        action = "systemctl reboot";
+        text = "Reboot";
+        keybind = "r";
+      }
+      {
+        label = "";
+        action = "true";
+        text = "";
+        keybind = "";
+      }
+    ];
   };
 }
