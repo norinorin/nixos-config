c626c6bfa9cedfd5b68db79b404c747149ef8ee0
Nori <norizon16@proton.me>
2025-12-02 23:39:12 +0700

    disable ppd

--- nixos/hibernation.nix
+++ nixos/hibernation.nix
@@ -10,7 +10,6 @@
   boot.kernelParams = ["resume_offset=75100160"];
   # FIXME: get the uuid from `lsblk -f`
   boot.resumeDevice = "/dev/disk/by-uuid/93d0ed76-31cb-4867-84bc-e06d273aa01c";
-  services.power-profiles-daemon.enable = true;
   powerManagement.enable = true;
   services.logind.settings.Login = {
     HandlePowerKey = "hibernate";
