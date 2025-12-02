c2cbdb44f8ee8b42788fd916cb843b509ef43c78
Nori <norizon16@proton.me>
2025-12-02 08:38:35 +0700

    feat: ppd

--- nixos/hibernation.nix
+++ nixos/hibernation.nix
@@ -1,4 +1,4 @@
-{
+{pkgs, ...}: {
   swapDevices = [
     {
       device = "/var/lib/swapfile";
@@ -10,9 +10,14 @@
   boot.kernelParams = ["resume_offset=75100160"];
   # FIXME: get the uuid from `lsblk -f`
   boot.resumeDevice = "/dev/disk/by-uuid/93d0ed76-31cb-4867-84bc-e06d273aa01c";
+  services.power-profiles-daemon.enable = true;
   powerManagement.enable = true;
   services.logind.settings.Login = {
     HandlePowerKey = "hibernate";
     HandlePowerKeyLongPress = "poweroff";
   };
+
+  environment.systemPackages = with pkgs; [
+    power-profiles-daemon
+  ];
 }
