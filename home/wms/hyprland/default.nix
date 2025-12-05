f60e611891ad3c73e2f19aa9a38d83301e8ebb3b
Nori <norizon16@proton.me>
2025-12-06 01:24:56 +0700

    hyprland: get rid of the patch

--- home/wms/hyprland/default.nix
+++ home/wms/hyprland/default.nix
@@ -6,18 +6,6 @@
   ...
 }: let
   inherit (pkgs.stdenv.hostPlatform) system;
-  pkgs-hypr = inputs.hyprland.packages.${pkgs.stdenv.hostPlatform.system};
-  hyprlandPatched = pkgs-hypr.hyprland.overrideAttrs (old: {
-    postPatch =
-      (old.postPatch or "")
-      + ''
-        sed -i 's/xkbcommon >=/xkbcommon >= /g' hyprland.pc.in
-      '';
-  });
-  mkPlugin = plugin:
-    plugin.overrideAttrs (old: {
-      buildInputs = [hyprlandPatched] ++ old.buildInputs;
-    });
   hyperPlugins = inputs.hyprland-plugins.packages.${system};
 
   mkWaybar = import ../lib.nix {inherit pkgs config lib;};
@@ -38,8 +26,8 @@ in {
     portalPackage = null;
     systemd.variables = ["--all"];
     plugins = [
-      (mkPlugin inputs.split-monitor-workspaces.packages.${pkgs.stdenv.hostPlatform.system}.split-monitor-workspaces)
-      (mkPlugin hyperPlugins.csgo-vulkan-fix)
+      inputs.split-monitor-workspaces.packages.${pkgs.stdenv.hostPlatform.system}.split-monitor-workspaces
+      hyperPlugins.csgo-vulkan-fix
     ];
     submaps = {
       move = {
