84cba82fc5cc76c4f41fdedcdef3582fbe8253a8
Nori <norizon16@proton.me>
2025-12-17 12:15:22 +0700

    feat(audio): diffuse field filters

--- nixos/audio.nix
+++ nixos/audio.nix
@@ -1,4 +1,4 @@
-{
+{pkgs, ...}: {
   services.pipewire = {
     enable = true;
     alsa.enable = true;
@@ -35,6 +35,34 @@
         "context.properties" = {
           "default.clock.allowed-rates" = [44100 48000 88200 96000];
         };
+      };
+
+      "20-parametric-eq" = {
+        "context.modules" = let
+          eqFilters = pkgs.writeText "parametric-eq.txt" ''
+            Preamp: -1.6 dB
+            Filter 1: ON PK Fc 20 Hz Gain -1.8 dB Q 1.900
+            Filter 2: ON PK Fc 40 Hz Gain -6.1 dB Q 0.500
+            Filter 3: ON PK Fc 44 Hz Gain 0.9 dB Q 1.700
+            Filter 4: ON PK Fc 160 Hz Gain -0.5 dB Q 0.900
+            Filter 5: ON PK Fc 510 Hz Gain 2.0 dB Q 1.000
+            Filter 6: ON PK Fc 1500 Hz Gain -2.2 dB Q 1.000
+            Filter 7: ON PK Fc 3000 Hz Gain 2.0 dB Q 2.000
+            Filter 8: ON PK Fc 4600 Hz Gain -2.5 dB Q 2.000
+            Filter 9: ON PK Fc 6000 Hz Gain 2.5 dB Q 2.000
+            Filter 10: OFF PK Fc 0 Hz Gain 0.0 dB Q 0.000
+          '';
+        in [
+          {
+            name = "libpipewire-module-parametric-equalizer";
+            args = {
+              "equalizer.filepath" = eqFilters;
+              "equalizer.description" = "DF Target Parametric EQ";
+              "audio.channels" = 2;
+              "audio.position" = ["FL" "FR"];
+            };
+          }
+        ];
       };
     };
   };
