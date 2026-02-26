c1fe3b75311b4dc61e5655de5027ed1179ecea02
Nori <norizon16@proton.me>
2026-02-27 00:39:36 +0700

    feat: sshd

--- nixos/default.nix
+++ nixos/default.nix
@@ -140,6 +140,28 @@ in {
         CPU_MAX_PERF_ON_BAT = 20;
       };
     };
+    openssh = {
+      enable = true;
+      settings = {
+        PermitRootLogin = "no";
+        PasswordAuthentication = true;
+        Macs = [
+          "hmac-sha2-512-etm@openssh.com"
+          "hmac-sha2-256-etm@openssh.com"
+          "umac-128-etm@openssh.com"
+
+          # Samsung MyFiles
+          "hmac-sha1"
+          "hmac-sha2-256"
+          "hmac-md5"
+        ];
+      };
+      extraConfig = ''
+        # Samsung MyFiles
+        HostKeyAlgorithms +ssh-rsa
+        PubkeyAcceptedAlgorithms +ssh-rsa
+      '';
+    };
   };
 
   networking.firewall = {
