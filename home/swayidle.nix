64d76e606c16c17441b8888ccb56968d609a1921
Nori <norizon16@proton.me>
2026-04-14 05:52:21 +0700

    fix(niri): use the correct niri package for ipc calls

--- home/swayidle.nix
+++ home/swayidle.nix
@@ -2,20 +2,21 @@
 {
   pkgs,
   lib,
+  config,
   ...
 }: {
   services.swayidle = let
     lock = "${pkgs.swaylock}/bin/swaylock --daemonize";
-    display = status: "${pkgs.niri}/bin/niri msg action power-${status}-monitors";
+    display = status: "${config.programs.niri.package}/bin/niri msg action power-${status}-monitors";
   in {
     enable = lib.mkDefault false;
     timeouts = [
       {
-        timeout = 295; # in seconds
-        command = "${pkgs.libnotify}/bin/notify-send 'Locking in 5 seconds' -t 5000";
+        timeout = 300;
+        command = display "off";
       }
       {
-        timeout = 300;
+        timeout = 310;
         command = lock;
       }
     ];
@@ -26,14 +27,6 @@
       }
       {
         event = "after-resume";
-        command = display "on";
-      }
-      {
-        event = "lock";
-        command = (display "off") + "; " + lock;
-      }
-      {
-        event = "unlock";
         command = display "on";
       }
     ];
