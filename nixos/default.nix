a666bdc1b8468d3b4b818b5aa862a1990429a424
Nori <norizon16@proton.me>
2026-04-14 22:56:21 +0700

    tlp: switch to unstable for tlp-pd

--- nixos/default.nix
+++ nixos/default.nix
@@ -42,6 +42,7 @@ in {
     ./overlays.nix
     ./gaming.nix
     ./sops.nix
+    ./tlp.nix
 
     (import ./shared {inherit config lib pkgs inputs displayManager;})
   ];
@@ -155,32 +156,6 @@ in {
     };
     udisks2.enable = true;
     thermald.enable = true;
-    tlp = {
-      enable = true;
-      settings = {
-        CPU_SCALING_GOVERNOR_ON_AC = "performance";
-        CPU_SCALING_GOVERNOR_ON_BAT = "powersave";
-
-        CPU_ENERGY_PERF_POLICY_ON_BAT = "power";
-        CPU_ENERGY_PERF_POLICY_ON_AC = "performance";
-
-        CPU_MIN_PERF_ON_AC = 0;
-        CPU_MAX_PERF_ON_AC = 100;
-        CPU_MIN_PERF_ON_BAT = 0;
-        CPU_MAX_PERF_ON_BAT = 20;
-
-        SOUND_POWER_SAVE_ON_AC = 0;
-        SOUND_POWER_SAVE_ON_BAT = 1;
-
-        PLATFORM_PROFILE_ON_AC = "balance";
-        PLATFORM_PROFILE_ON_BAT = "low-power";
-
-        RUNTIME_PM_ON_AC = "on";
-        RUNTIME_PM_ON_BAT = "auto";
-        PCIE_ASPM_ON_AC = "default";
-        PCIE_ASPM_ON_BAT = "powersupersave";
-      };
-    };
     openssh = {
       enable = true;
       settings = {
