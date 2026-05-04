f0b6a6ce04d936cfd36ed4a896a97c9ff03d9f86
Nori <norizon16@proton.me>
2026-05-04 22:47:30 +0700

    toaster: make HDMI-A-5 persistent

--- modules/hosts/toaster/default.nix
+++ modules/hosts/toaster/default.nix
@@ -7,6 +7,12 @@
     nixos = {pkgs, ...}: {
       imports = [./_hardware-configuration.nix];
 
+      # make HDMI-A-5 persistent
+      # this fixes a bug in niri where the context menu in firefox
+      # is rendered offscreen (invisible) after the connector HDMI-A-5
+      # gets reinitialised on dpms off/on (which happens regularly on idle)
+      boot.kernelParams = ["video=HDMI-A-5:e"];
+
       virtualisation.vmVariant = {
         imports = [(den.provides.tty-autologin "nori").nixos];
 
