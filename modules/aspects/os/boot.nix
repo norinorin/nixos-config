e7453245c6d013699c5deb1482c70b5f0ee32dc1
Nori <norizon16@proton.me>
2026-05-07 04:16:37 +0700

    kernel: switch to xanmod latest

--- modules/aspects/os/boot.nix
+++ modules/aspects/os/boot.nix
@@ -9,7 +9,7 @@
 
       boot.loader.systemd-boot.enable = true;
       boot.loader.efi.canTouchEfiVariables = true;
-      boot.kernelPackages = pkgs.linuxPackages_zen;
+      boot.kernelPackages = pkgs.linuxPackages_xanmod_latest;
       boot.zswap = {
         enable = true;
         compressor = "zstd";
