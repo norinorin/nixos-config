7fceed00734839af256e6f028c77c1177efb7123
Nori <norizon16@proton.me>
2026-05-08 05:10:22 +0700

    nixcord: update flake
    Latest commit exposes graphics lib to the official Discord client

--- modules/aspects/desktop/messaging/nixcord.nix
+++ modules/aspects/desktop/messaging/nixcord.nix
@@ -2,27 +2,13 @@
   flake-file.inputs.nixcord.url = "github:FlameFlag/nixcord/dev";
 
   den.aspects.nixcord = {
-    homeManager = {
-      pkgs,
-      nixcordPkgs,
-      ...
-    }: {
+    homeManager = {pkgs, ...}: {
       imports = [inputs.nixcord.homeModules.nixcord];
       programs.nixcord = {
         enable = true;
         discord = {
           vencord.enable = false;
           equicord.enable = true;
-          package = nixcordPkgs.discord.overrideAttrs (old: {
-            nativeBuildInputs = (old.nativeBuildInputs or []) ++ [pkgs.makeWrapper];
-
-            postFixup =
-              (old.postFixup or "")
-              + ''
-                wrapProgram $out/opt/Discord/Discord \
-                  --prefix LD_LIBRARY_PATH : "/run/opengl-driver/lib"
-              '';
-          });
         };
         equibop.enable = true;
         config = {
